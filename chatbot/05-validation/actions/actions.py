cd039ec1229286e8a8dd28dd3cb5f8b1bbdf7677
Dingdong-LIU <dliuak@connect.ust.hk>
2023-02-26 22:05:33 +0800

    initial setup for chatbots, including code samples for actions, slots, conditions, text and validation. Also an init chatbot is included in README.md

---
+++ chatbot/05-validation/actions/actions.py
@@ -0,0 +1,50 @@
+from typing import Text, List, Any, Dict
+
+from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+
+
+def clean_name(name):
+    return "".join([c for c in name if c.isalpha()])
+
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_name_form"
+
+    def validate_first_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `first_name` value."""
+
+        # If the name is super short, it might be wrong.
+        name = clean_name(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"first_name": None}
+        return {"first_name": name}
+
+    def validate_last_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `last_name` value."""
+
+        # If the name is super short, it might be wrong.
+        name = clean_name(slot_value)
+        if len(name) == 0:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"last_name": None}
+        
+        first_name = tracker.get_slot("first_name")
+        if len(first_name) + len(name) < 3:
+            dispatcher.utter_message(text="That's a very short name. We fear a typo. Restarting!")
+            return {"first_name": None, "last_name": None}
+        return {"last_name": name}
